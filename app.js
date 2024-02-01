a2bb1345c92c0d030f6cac548b0c96b91102592a
vinayak9591 <vinayaknatikar123@gmail.com>
2024-02-01 23:08:55 +0530

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express=require('express');
 const app=express();
 
@@ -12,6 +11,3 @@ const PORT = process.env.PORT || 3000 ;
 app.listen(PORT,() =>{
     console.log(`server started on port ${PORT} in ${process.env.NODE_ENV} mode.`);
 });
-=======
-console.log('vinayak');
->>>>>>> 23a8e46d0260375bf8ec2044a51318f71f80456d

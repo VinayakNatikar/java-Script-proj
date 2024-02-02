2507c056d54a897598f7a0813a545df31c8d9e5e
vinayak9591 <vinayaknatikar123@gmail.com>
2024-02-02 19:12:07 +0530

    third commit

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express=require('express');
 const app=express();
 
 const dotenv=require('dotenv');
+
+const connectDatabase=require('./config/database');
 dotenv.config({path : './config/config.env'  });
+
+connectDatabase();
 
 const jobs=require('./routes/jobs');
 app.use(jobs);
